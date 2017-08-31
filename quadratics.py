37f0a317e7db81c21c9411bebfa04e22cd271c8e
Resham1458 <anuresham2015@gmail.com>
2017-08-31 13:37:17 -0400

    uploaded

---
+++ quadratics.py
@@ -0,0 +1,8 @@
+print("Welcome to my quadratic program!")
+A= int(input("enter the value of A"))
+B= int(input("enter the value of B"))
+C= int(input("enter the value of C"))
+X= int(input("enter the value of X"))
+quadratic = (A*(X*X))+(B*X)+C
+print("The quadratic you entered is=",A,"X^2","+",B,"X","+",C ) # displays quadratic
+print("The value of your quadratic is=",quadratic) 				# outputs quadratic value
